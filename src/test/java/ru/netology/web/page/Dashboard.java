a49d832f8da1995bc5c850e3fb8b32bc8bbc0969
EkaterinaPeregudova <katerina-kis@yandex.ru>
2021-11-04 10:41:14 +0400

    Page classes and DataHelper updated

--- src/test/java/ru/netology/web/page/Dashboard.java
+++ src/test/java/ru/netology/web/page/Dashboard.java
@@ -1,4 +1,13 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
 public class Dashboard {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+
+    public Dashboard(){
+        heading.shouldBe(visible);
+    };
 }
